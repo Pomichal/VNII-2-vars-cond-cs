2f1db9f27bccc7ed34cfcd22b13858960fbf784a
Pomichal <vajk.pomichal@gmail.com>
2023-02-20 15:28:31 +0100

    ADD: initial setup

---
+++ Assets/Intro.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+
+    public int x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(x);
+        print("Hello world!");
+    }
+}
